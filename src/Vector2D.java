fc2e49b93df5ad3727590d6c6eb036a8a92e359f
Artem Tenhsi <zakan.6000@gmail.com>
2022-10-31 12:26:28 +0800

    Vector2d init

---
+++ src/Vector2D.java
@@ -0,0 +1,58 @@
+import java.util.Locale;
+
+public class Vector2D {
+
+    static double count;
+
+    public double Vx, Vy;
+
+    public Vector2D(){
+        this.Vx = 1;
+        this.Vy = 1;
+        count++;
+    }
+
+    public Vector2D(double Vx, double Vy){
+        this.Vx = Vx;
+        this.Vy = Vy;
+        count++;
+    }
+
+    public Vector2D(Vector2D v){
+        this.Vx = v.Vx;
+        this.Vy = v.Vy;
+        count++;
+    }
+
+    public void print(){
+        System.out.println("(" + String.format(Locale.US, "%.2f", Vx) + ", " + String.format(Locale.US, "%.2f", Vy) + ")");
+    }
+
+    public void add(Vector2D v){
+        this.Vx = Vx + v.Vx;
+        this.Vy = Vy + v.Vy;
+    }
+    public void sub(Vector2D v){
+        this.Vx = Vx - v.Vx;
+        this.Vy = Vy - v.Vy;
+    }
+
+    public void scale(double scaleFactor){
+        this.Vx = Vx * scaleFactor;
+        this.Vy = Vy * scaleFactor;
+    }
+
+    public double length(){
+        return Math.sqrt((Vx * Vx) + (Vy * Vy));
+    }
+
+    public void normalized(){
+        this.Vx = Vx / length();
+        this.Vy = Vy / length();
+    }
+
+    public double dotProduct(Vector2D v){
+        return Vx * v.Vx + Vy * v.Vy;
+    }
+
+}
